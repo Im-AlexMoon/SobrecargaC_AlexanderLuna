a7b540b6664aec9ce2faa9d9f536b561395ebb56
Im-AlexMoon <A00574602@tec.mx>
2025-05-16 22:24:28 -0600

    Update ApDirecta_Ejercicio1.cpp

--- Sobrecarga_AlexanderLuna/Soluciones a ejercicios/Soluciones/ApDirecta_Ejercicio1.cpp
+++ Sobrecarga_AlexanderLuna/Soluciones a ejercicios/Soluciones/ApDirecta_Ejercicio1.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+class Complejo {
+public:
+    double re, im;
+
+    Complejo(double r = 0.0, double i = 0.0) : re(r), im(i) {}
+
+    // Sobrecarga del operador +
+    Complejo operator+(const Complejo& otro) const {
+        return Complejo(re + otro.re, im + otro.im);
+    }
+};
+
+int main() {
+    Complejo a(1.0, 2.0), b(3.0, -1.0);
+    Complejo c = a + b;
+    cout << "(" << c.re << ", " << c.im << ")" << endl;   // (4.0, 1.0)
+    return 0;
+}
