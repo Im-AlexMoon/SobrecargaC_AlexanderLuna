fb082a2b1ca7eebd51e4381d4971afee02835ea6
Im-AlexMoon <A00574602@tec.mx>
2025-05-16 22:57:53 -0600

    Create Completar_Ejercicio4.cpp

---
+++ Sobrecarga_AlexanderLuna/Soluciones a ejercicios/Soluciones/Completar_Ejercicio4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Contador{
+    int valor;
+public:
+    Contador(int v): valor(v) {}
+    Contador operator++(int){            // completado
+        Contador temp = *this;           
+        ++valor;                     
+        return temp;                     
+    }
+    int get() const { return valor; }
+};
+
+int main(){
+    Contador cnt(10);
+    Contador old = cnt++;
+    cout << "Old: " << old.get() << ", New: " << cnt.get() << endl;
+}
